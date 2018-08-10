4a9ab4fc93a6316956e67ce90d42f27719563b81
Joseph Stewart <joseph.s@nationalschoolproject.com>
2018-08-10 14:02:22 -0700

    Design review from hannah

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASS
+	}
+});
 
 var app = express();
 
@@ -13,6 +22,20 @@ app.get(/images\/(.+)/, (request, response) => {
 
 app.use(express.static(__dirname + '/../hannahs_server/dist'));
 
+app.post('/contact-form', (request, response) => {
+	const mailOptionsClient = {
+		from: process.env.EMAIL_USER,
+		to: request.body.email,
+		subject: 'MLC Designs',
+		html: `<p><strong>Name:</strong> ${request.body.name}<br/><strong>Message:</strong> ${request.body.message}</p>`
+	}
+
+	transporter.sendMail(mailOptionsClient, err => {
+		if (err) response.redirect('/error.html');
+		else response.redirect('/success.html');
+	});
+})
+
 app.use((request, response, next) => {
 	response.header('Access-Control-Allow-Origin', '*');
 	response.header('Access-Control-Allow-Methods', 'GET');
@@ -25,4 +48,5 @@ app.route("*").get((request, response) => {
 	response.redirect('/');
 });
 
+app.listen(8080);
 module.exports = app;
